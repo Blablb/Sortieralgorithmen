f62337efd2a1ebc8fb712f4f95ec1c47f523d661
BVTJohnn <pauloweber26@yahoo.de>
2021-05-17 18:23:10 +0200

    Quick_Sort

---
+++ Qucik_Sort.js
@@ -0,0 +1,30 @@
+function quick_Sort(origArray) {
+	if (origArray.length <= 1) { 
+		return origArray;
+	} else {
+
+		var left = [];
+		var right = [];
+		var newArray = [];
+		var pivot = origArray.pop();
+		var length = origArray.length;
+
+		for (var i = 0; i < length; i++) {
+			if (origArray[i] <= pivot) {
+				left.push(origArray[i]);
+			} else {
+				right.push(origArray[i]);
+			}
+		}
+
+		return newArray.concat(quick_Sort(left), pivot, quick_Sort(right));
+	}
+}
+
+var myArray = [32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1];
+
+console.log("Original array: " + myArray);
+console.time();
+var sortedArray = quick_Sort(myArray);
+console.timeEnd();
+console.log("Sorted array: " + sortedArray);
